759511731bd3898c502f09664bf00b24a456e32e
Satyam Kashyap <Laughalot>
2021-05-04 20:18:26 +0530

    diamonds completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 function App() {
-  const [prediction, setPrediction] = useState({})
+  const [prediction, setPrediction] = useState({
+    0: { name: "As", zodiac: "Gemini", rasi_no: 3, house: 1 },
+  1: { name: "Su", zodiac: "Aquarius", rasi_no: 11, house: 9, retro: false },
+  2: { name: "Mo", zodiac: "Sagittarius", rasi_no: 9, house: 7, retro: false },
+  3: { name: "Ma", zodiac: "Aries", rasi_no: 1, house: 11, retro: false },
+  4: { name: "Me", zodiac: "Cancer", rasi_no: 4, house: 2, retro: false },
+  5: { name: "Ju", zodiac: "Pisces", rasi_no: 12, house: 10, retro: true },
+  6: { name: "Ve", zodiac: "Aries", rasi_no: 1, house: 11, retro: false },
+  7: { name: "Sa", zodiac: "Capricorn", rasi_no: 10, house: 8, retro: true },
+  8: { name: "Ra", zodiac: "Aries", rasi_no: 1, house: 11, retro: true },
+    9: { name: "Ke", zodiac: "Libra", rasi_no: 7, house: 5, retro: true },
+    10: { name: "A2", zodiac: "Gemini", rasi_no: 5, house: 1 },
+    11: { name: "A3", zodiac: "Gemini", rasi_no: 5, house: 1 },
+    12: { name: "A4", zodiac: "Gemini", rasi_no: 5, house: 1 },
+    13: { name: "A5", zodiac: "Gemini", rasi_no: 5, house: 1 },
+    14: { name: "A6", zodiac: "Gemini", rasi_no: 5, house: 1 },
+    15: { name: "A7", zodiac: "Gemini", rasi_no: 5, house: 1 },
+    16: { name: "A8", zodiac: "Gemini", rasi_no: 5, house: 1 },
+    17: { name: "A9", zodiac: "Gemini", rasi_no: 5, house: 1 },
+    18: { name: "A2", zodiac: "Gemini", rasi_no: 6, house: 7 },
+    19: { name: "A3", zodiac: "Gemini", rasi_no: 9, house: 7 },
+    20: { name: "A4", zodiac: "Gemini", rasi_no: 7, house: 7 },
+    22: { name: "A7", zodiac: "Gemini", rasi_no: 7, house: 7 },
+    22: { name: "A6", zodiac: "Gemini", rasi_no: 7, house: 7 },
+    23: { name: "A7", zodiac: "Gemini", rasi_no: 7, house: 7 },
+    24: { name: "A8", zodiac: "Gemini", rasi_no: 7, house: 7 },
+    27: { name: "A9", zodiac: "Gemini", rasi_no: 7, house: 7 },
+    28: { name: "A1", zodiac: "Gemini", rasi_no: 7, house: 7 },
+  chart: "D9"
+  })
   const [isLoading, setLoading] = useState(false)
   var str = require("./chartMaker")
-  useEffect(() => {
+  /*useEffect(() => {
 
     var params = {
-      dob: "04/11/2001",
+      dob: "20/01/2001",
       tob: "07:30",
       lat: 11.27,
       lon: 78.27,
@@ -20,21 +49,18 @@ function App() {
       const result = await axios(
         'https://api.vedicastroapi.com/json/horoscope/divisionalcharts', { params: params }
       );
-      console.log(result.data.response)
       setPrediction(result.data.response);
       setLoading(true)
     }
     fetchData()}
       ,[]);
-  
+  */
   return (
     <div className="App">
-      
-        {console.log(prediction, "ptrresxd")}
-        
-      <img src={`data:image/svg+xml;utf8,${isLoading ? str(prediction, "north", "blue") : ''}`} />
-        
-     
+      {//console.log(prediction)
+      }
+      <img src={`data:image/svg+xml;utf8,${str(prediction, "north", "blue")}`} alt="kundli"/> 
+      <img src={`data:image/svg+xml;utf8,${isLoading ? str(prediction, "north", "blue") : ''}`} alt="kundli"/>
     </div>
   );
 }
--- src/chartMaker.js
+++ src/chartMaker.js
@@ -1,139 +1,395 @@
 module.exports = (newjson, style, color) => {
-    var house = [[], [], [], [], [], [], [], [], [], [], [], []], rasi = [[], [], [], [], [], [], [], [], [], [], [], []], rasi_no = [1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12], new_rasi_no = []
-    var svgStringHouse = ""
-    var svgStringRasi = ""
+  var house = [[], [], [], [], [], [], [], [], [], [], [], []],
+    rasi = [[], [], [], [], [], [], [], [], [], [], [], []],
+    rasi_no = [1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    new_rasi_no = [];
+  var svgStringHouse = "";
+  var svgStringRasi = "";
 
-    // Location for printing each of the houses in the SVG
-    var houseAddresses = [
-        [[240, 125], [210, 100], [280, 100], [210, 130], [280, 130], [210, 160], [280, 160], [245, 190], [245, 40]],
-        [[60, 25], [100, 25], [140, 25], [180, 25], [80, 50], [120, 50], [160, 50], [120, 75], [25, 25]],
-        [[10, 70], [10, 110], [10, 150], [10, 190], [50, 90], [50, 130], [50, 170], [50, 130], [10, 40]],
-        [[120, 180], [80, 220], [160, 220], [80, 260], [160, 260], [80, 300], [160, 300], [120, 340], [150, 180]],
+  // Location for printing each of the houses in the SVG
+  var houseAddresses = [
+    [
+      [125, 125],                         // House 1
+      [125 + 250*50/100, 250*25/100],     // first element is the origin
+      [125 + 250*50/100, 250*75/100],     //rest elements are cordinates of elements from 4 to 9
+      [125 + 250*37.5/100, 250*62.5/100], 
+      [125 + 250*62.5/100, 250*62.5/100],  
+      [125 + 250*37.5/100, 250*37.5/100],
+      [125 + 250*62.5/100, 250*37.5/100],
+      ],
+      [
+        [60, 25],
+        [100, 25],
+        [140, 25],
+        [10, 25],
+        [80, 50],
+        [120, 50],
+        [160, 50],
+        [120, 75],
+        [25, 25],
+      ],
+      [
+        [10, 70],
+        [10, 110],
+        [10, 150],
+        [10, 190],
+        [50, 90],
+        [50, 130],
+        [50, 170],
+        [50, 130],
+        [10, 40],
+      ],
+      [
+        [0, 250], 
+        [ 250*50/100, 125 + 250*25/100],
+        [250*50/100, 125 + 250*75/100],
+        [250*37.5/100, 125 +  250*62.5/100],
+        [250*62.5/100, 125 + 250*62.5/100],
+        [250*37.5/100, 125 + 250*37.5/100],
+        [250*62.5/100, 125 + 250*37.5/100],
+        ],
 
-        [[10, 320], [10, 360], [10, 400], [10, 440], [50, 350], [50, 390], [50, 430], [80, 380], [10, 285]],
-        [[60, 485], [100, 485], [140, 485], [180, 485], [80, 460], [120, 460], [160, 460], [120, 435], [30, 485]],
-        [[245, 320], [210, 350], [280, 350], [210, 380], [280, 380], [210, 410], [280, 410], [245, 440], [245, 480]],
-        [[470, 320], [470, 360], [470, 400], [470, 440], [440, 350], [440, 390], [440, 430], [410, 380], [470, 290]],
+      [
+        [10, 320],
+        [10, 360],
+        [10, 400],
+        [10, 440],
+        [50, 350],
+        [50, 390],
+        [50, 430],
+        [80, 380],
+        [10, 285],
+      ],
+      [
+        [60, 485],
+        [100, 485],
+        [140, 485],
+        [180, 485],
+        [80, 460],
+        [120, 460],
+        [160, 460],
+        [120, 435],
+        [30, 485],
+      ],
+      [
+        [125, 375], 
+        [125 + 250*50/100, 250 + 250*25/100],
+        [125 + 250*50/100, 250 + 250*75/100],
+        [125 + 250*37.5/100, 250 +  250*62.5/100],
+        [125 + 250*62.5/100, 250 + 250*62.5/100],
+        [125 + 250*37.5/100, 250 + 250*37.5/100],
+        [125 + 250*62.5/100, 250 + 250*37.5/100],
+        ],
+      [
+        [470, 320],
+        [470, 360],
+        [470, 400],
+        [470, 440],
+        [440, 350],
+        [440, 390],
+        [440, 430],
+        [410, 380],
+        [470, 290],
+      ],
 
-        [[300, 485], [340, 485], [380, 485], [420, 485], [335, 460], [375, 460], [415, 460], [375, 435], [275, 490]],
-        [[375, 180], [335, 220], [415, 220], [335, 260], [415, 260], [335, 300], [415, 300], [375, 340], [470, 250]],
-        [[470, 70], [470, 110], [470, 150], [470, 190], [440, 90], [440, 130], [440, 170], [410, 130], [470, 40]],
-        [[300, 25], [340, 25], [380, 25], [420, 25], [335, 50], [375, 50], [415, 50], [375, 75], [275, 20]]
-    ], rasiAddresses = [
-        [[150, 40], [180, 40], [210, 40], [150, 80], [180, 80], [210, 80], [150, 120], [180, 120], [210, 120]],
-        [[270, 40], [300, 40], [330, 40], [270, 80], [300, 80], [330, 80], [270, 120], [300, 120], [330, 120]],
-        [[400, 40], [430, 40], [460, 40], [400, 80], [430, 80], [460, 80], [400, 120], [430, 120], [460, 120]],
-        [[400, 160], [430, 160], [460, 160], [400, 190], [430, 190], [460, 190], [400, 210], [430, 210], [460, 210]],
-        [[400, 280], [430, 280], [460, 280], [400, 310], [430, 310], [460, 310], [400, 340], [430, 340], [460, 340]],
-        [[400, 410], [430, 410], [460, 410], [400, 440], [430, 440], [460, 440], [400, 470], [430, 470], [460, 470]],
-        [[270, 410], [300, 410], [330, 410], [270, 440], [300, 440], [330, 440], [270, 470], [300, 470], [330, 470]],
-        [[150, 410], [180, 410], [210, 410], [150, 440], [180, 440], [210, 440], [150, 470], [180, 470], [210, 470]],
-        [[25, 410], [55, 410], [85, 410], [25, 440], [55, 440], [85, 440], [25, 470], [55, 470], [85, 470]],
-        [[25, 280], [55, 280], [85, 280], [25, 310], [55, 310], [85, 310], [25, 340], [55, 340], [85, 340]],
-        [[25, 160], [55, 160], [85, 160], [25, 190], [55, 190], [85, 190], [25, 210], [55, 210], [85, 210]],
-        [[25, 40], [55, 40], [85, 40], [25, 80], [55, 80], [85, 80], [25, 120], [55, 120], [85, 120]],
-    ], rasi_no = [1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12], new_rasi_no = []
+      [
+        [300, 485],
+        [340, 485],
+        [380, 485],
+        [420, 485],
+        [335, 460],
+        [375, 460],
+        [415, 460],
+        [375, 435],
+        [275, 490],
+      ],
+      [
+        [250, 250], 
+        [250 + 250*50/100, 125 + 250*25/100],
+        [250 + 250*50/100, 125 + 250*75/100],
+        [250 + 250*37.5/100,125 + 250*62.5/100],
+        [250 + 250*62.5/100,125 + 250*62.5/100],
+        [250 + 250*37.5/100,125 + 250*37.5/100],
+        [250 + 250*62.5/100,125 + 250*37.5/100],
+        ],
+      [
+        [470, 70],
+        [470, 110],
+        [470, 150],
+        [470, 190],
+        [440, 90],
+        [440, 130],
+        [440, 170],
+        [410, 130],
+        [470, 40],
+      ],
+      [
+        [300, 25],
+        [340, 25],
+        [380, 25],
+        [420, 25],
+        [335, 50],
+        [375, 50],
+        [415, 50],
+        [375, 75],
+        [275, 20],
+      ],
+    ],
+    rasiAddresses = [
+      [
+        [150, 40],
+        [180, 40],
+        [210, 40],
+        [150, 80],
+        [180, 80],
+        [210, 80],
+        [150, 120],
+        [180, 120],
+        [210, 120],
+      ],
+      [
+        [270, 40],
+        [300, 40],
+        [330, 40],
+        [270, 80],
+        [300, 80],
+        [330, 80],
+        [270, 120],
+        [300, 120],
+        [330, 120],
+      ],
+      [
+        [400, 40],
+        [430, 40],
+        [460, 40],
+        [400, 80],
+        [430, 80],
+        [460, 80],
+        [400, 120],
+        [430, 120],
+        [460, 120],
+      ],
+      [
+        [400, 160],
+        [430, 160],
+        [460, 160],
+        [400, 190],
+        [430, 190],
+        [460, 190],
+        [400, 210],
+        [430, 210],
+        [460, 210],
+      ],
+      [
+        [400, 280],
+        [430, 280],
+        [460, 280],
+        [400, 310],
+        [430, 310],
+        [460, 310],
+        [400, 340],
+        [430, 340],
+        [460, 340],
+      ],
+      [
+        [400, 410],
+        [430, 410],
+        [460, 410],
+        [400, 440],
+        [430, 440],
+        [460, 440],
+        [400, 470],
+        [430, 470],
+        [460, 470],
+      ],
+      [
+        [270, 410],
+        [300, 410],
+        [330, 410],
+        [270, 440],
+        [300, 440],
+        [330, 440],
+        [270, 470],
+        [300, 470],
+        [330, 470],
+      ],
+      [
+        [150, 410],
+        [180, 410],
+        [210, 410],
+        [150, 440],
+        [180, 440],
+        [210, 440],
+        [150, 470],
+        [180, 470],
+        [210, 470],
+      ],
+      [
+        [25, 410],
+        [55, 410],
+        [85, 410],
+        [25, 440],
+        [55, 440],
+        [85, 440],
+        [25, 470],
+        [55, 470],
+        [85, 470],
+      ],
+      [
+        [25, 280],
+        [55, 280],
+        [85, 280],
+        [25, 310],
+        [55, 310],
+        [85, 310],
+        [25, 340],
+        [55, 340],
+        [85, 340],
+      ],
+      [
+        [25, 160],
+        [55, 160],
+        [85, 160],
+        [25, 190],
+        [55, 190],
+        [85, 190],
+        [25, 210],
+        [55, 210],
+        [85, 210],
+      ],
+      [
+        [25, 40],
+        [55, 40],
+        [85, 40],
+        [25, 80],
+        [55, 80],
+        [85, 80],
+        [25, 120],
+        [55, 120],
+        [85, 120],
+      ],
+    ],
+    rasi_no = [1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    new_rasi_no = [];
 
-    var startRasi = newjson[0].rasi_no
+  var startRasi = newjson[0].rasi_no;
 
-    rasi_no.forEach((ele, index) => {
-
-        if (index + startRasi > 12) {
-            new_rasi_no.push((index + startRasi) % 12)
-        } else {
-            new_rasi_no.push(index + startRasi)
-        }
-
-    })
-
-
-
-    for (var key in newjson) {
-        console.log(key)
-
-
-        if (!isNaN(key)) {
-
-            if ((key != 8 && key != 9) && newjson[key].retro == true) {
-                // console.log("retro",newjson[key].rasi_no)
-
-                house[newjson[key].house - 1].push(`${newjson[key].name}(R)`)
-                rasi[newjson[key].rasi_no - 1].push(`${newjson[key].name}(R)`)
-            } else {
-                // console.log("Whyyy",newjson[key].rasi_no)
-
-                house[newjson[key].house - 1].push(newjson[key].name)
-                rasi[newjson[key].rasi_no - 1].push(newjson[key].name)
-            }
-        }
-
-
+  rasi_no.forEach((ele, index) => {
+    if (index + startRasi > 12) {
+      new_rasi_no.push((index + startRasi) % 12);
+    } else {
+      new_rasi_no.push(index + startRasi);
     }
+  });
 
+  for (var key in newjson) {
+    if (!isNaN(key)) {
+      if (key != 8 && key != 9 && newjson[key].retro == true) {
+        // console.log("retro",newjson[key].rasi_no)
 
-console.log(house, "house")
-    house.forEach((element, ind) => {
+        house[newjson[key].house - 1].push(`${newjson[key].name}(R)`);
+        rasi[newjson[key].rasi_no - 1].push(`${newjson[key].name}(R)`);
+      } else {
+        // console.log("Whyyy",newjson[key].rasi_no)
+
+        house[newjson[key].house - 1].push(newjson[key].name);
+        rasi[newjson[key].rasi_no - 1].push(newjson[key].name);
+      }
+    }
+  }
+
+  console.log(house, "house");
+  house.forEach((element, ind) => {
+    // there are 5 types of houses: full square(1, 4, 7, 10), upper triangle(6, 9), lower triangle(2, 12), right triangle(3, 5), left triangle(11, 8)
+    //everyone follows different or opposite pattern
+    // element is a list of elements in each house
+    //console.log(element, ind, "element");
+
+    // for whole square house
+    if (ind == 0 || ind == 3 || ind == 6 || ind == 9) {
         element.forEach((houseelem, Hindex) => {
-
-
-            svgStringHouse = svgStringHouse + `<text x="${houseAddresses[ind][Hindex][0]}" y= "${houseAddresses[ind][Hindex][1]}" style = "font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;display: flex;justify-content: center;align-items: center;color:rgb(83, 83, 83);font-weight:bold ">${houseelem}</text>`
-
-
-        });
-    });
-    rasi.forEach((element, ind) => {
-        element.forEach((houseelem, Hindex) => {
-            let isBold = false
-            if (houseelem == "As") {
-                isBold = true
+            // for dynamic positions
+            if (Hindex < 3) {
+            
+                svgStringHouse =
+                    svgStringHouse +
+                    `<text x="${houseAddresses[ind][0][0] + (250 / (element.length < 4 ? element.length + 1 : 4)) * (Hindex + 1 < 4 ? Hindex + 1 : 2)
+                    - 10}" y= "${houseAddresses[ind][0][1] + 5
+                    }" style = "font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;display: flex;justify-content: center;align-items: center;color:rgb(83, 83, 83);font-weight:bold ">${houseelem}</text>`;
             }
-
-
-            svgStringRasi = svgStringRasi + `<text x="${rasiAddresses[ind][Hindex][0]}" y= "${rasiAddresses[ind][Hindex][1]}" style = "font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;display: flex;justify-content: center;align-items: center;color:rgb(83, 83, 83)${isBold ? ";font-weight:bold" : ""} ">${houseelem}</text>`
-
-
+            else {   // for fixed positions
+                svgStringHouse =
+                    svgStringHouse +
+                    `<text x="${houseAddresses[ind][Hindex - 2][0] - 10}" y= "${houseAddresses[ind][Hindex - 2][1] + 5}" style = "font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;display: flex;justify-content: center;align-items: center;color:rgb(83, 83, 83);font-weight:bold ">${houseelem}</text>`;
+            }
         });
-    });
-
-    var themeColor = "rgb(0, 0, 0)"
-
-    switch (color) {
-        case "blue":
-            themeColor = "rgb(0, 164, 255)"
-            break;
-            case "dark_blue":
-            themeColor = "rgb(0, 42, 138)"
-            break;
-        case "yellow":
-            themeColor = "rgb(255, 214, 12)"
-            break;
-        case "green":
-            themeColor = "rgb(39, 220, 28)"
-            break;
-        case "dark_green":
-            themeColor = "rgb(8, 134, 70)"
-            break;
-        case "brown":
-            themeColor = "rgb(121, 68, 13)"
-            break;
         
-        case "orange":
-            themeColor = "rgb(255, 144, 29)"
-            break;
-        case "grey":
-            themeColor = "rgb(126, 136, 150)"
-            break;
-        case "red":
-            themeColor = "rgb(255, 117, 117)"
-            break;
-
-        default:
-            themeColor = "rgb(0, 0, 0)"
-            break;
+          
+      }
+     else {
+      element.forEach((houseelem, Hindex) => {
+        // houseelem = As / mo / ju etc
+        svgStringHouse =
+          svgStringHouse +
+          `<text x="${houseAddresses[ind][Hindex][0]}" y= "${houseAddresses[ind][Hindex][1]}" style = "font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;display: flex;justify-content: center;align-items: center;color:rgb(83, 83, 83);font-weight:bold ">${houseelem}</text>`;
+      });
     }
+  });
+  rasi.forEach((element, ind) => {
+    element.forEach((houseelem, Hindex) => {
+      let isBold = false;
+      if (houseelem == "As") {
+        isBold = true;
+      }
 
-if(style == "south"){
+      svgStringRasi =
+        svgStringRasi +
+        `<text x="${rasiAddresses[ind][Hindex][0]}" y= "${
+          rasiAddresses[ind][Hindex][1]
+        }" style = "font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;display: flex;justify-content: center;align-items: center;color:rgb(83, 83, 83)${
+          isBold ? ";font-weight:bold" : ""
+        } ">${houseelem}</text>`;
+    });
+  });
 
+  var themeColor = "rgb(0, 0, 0)";
+
+  switch (color) {
+    case "blue":
+      themeColor = "rgb(0, 164, 255)";
+      break;
+    case "dark_blue":
+      themeColor = "rgb(0, 42, 138)";
+      break;
+    case "yellow":
+      themeColor = "rgb(255, 214, 12)";
+      break;
+    case "green":
+      themeColor = "rgb(39, 220, 28)";
+      break;
+    case "dark_green":
+      themeColor = "rgb(8, 134, 70)";
+      break;
+    case "brown":
+      themeColor = "rgb(121, 68, 13)";
+      break;
+
+    case "orange":
+      themeColor = "rgb(255, 144, 29)";
+      break;
+    case "grey":
+      themeColor = "rgb(126, 136, 150)";
+      break;
+    case "red":
+      themeColor = "rgb(255, 117, 117)";
+      break;
+
+    default:
+      themeColor = "rgb(0, 0, 0)";
+      break;
+  }
+
+  if (style == "south") {
     var str = `<?xml version="1.0" encoding="iso-8859-1"?>
     <!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
     <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
@@ -154,14 +410,9 @@ if(style == "south"){
             ${svgStringRasi}
        
             </svg>
- `
-
-}else{
-
-
-
-
- var str = `<?xml version="1.0" encoding="iso-8859-1"?>
+ `;
+  } else {
+    var str = `<?xml version="1.0" encoding="iso-8859-1"?>
  <!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
  <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
  <svg height="500" width="500" xmlns="http://www.w3.org/2000/svg">
@@ -204,8 +455,7 @@ if(style == "south"){
        
   
  
-`
-    }
-    console.log(str)
-    return (str)
-}
+`;
+  }
+  return str;
+};
